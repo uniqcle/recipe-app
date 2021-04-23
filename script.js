f07504661ff7f5dfb7611e2d5c58ee4ebaa1841c
uniqcle <uniqcle@yandex.ru>
2021-04-23 10:59:53 +0700

    избранное кнопка

--- script.js
+++ script.js
@@ -32,9 +32,21 @@ function addMeal(mealData, random = false){
                 </div>
                 
                 <div class="meal-body">
-                    <h4>${mealData.Meal}</h4>
+                    <h4>${mealData.strMeal}</h4>
                     <button class = "fav-btn active"><i class="fas fa-heart "></i></button>
                 </div>
     `; 
+
+    const btn =  meal.querySelector('.meal-body .fav-btn'); 
+    
+    btn.addEventListener("click", ()=>{
+        btn.classList.toggle('active'); 
+
+    })
+    
+    
+
     meals.appendChild(meal); 
+
+        
 }
